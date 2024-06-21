cf7c1b823fccc45d39f3cff98b3c513a44dfe57a
nidish40 <nidishts@gmail.com>
2024-06-21 16:18:32 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detial: sys):
@@ -22,6 +22,5 @@ class CustomExcpetion(Exception):
     def __str__(self):
         return self.error_message
     
-
 
 
